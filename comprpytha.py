ef0850bbcaae1ff7d21f22496e9e3833fb9f4d31
Abou Daman <adaman2000@gmail.com>
2017-07-05 15:24:38 -0400

    Code to compute pythagoras equation

---
+++ comprpytha.py
@@ -0,0 +1,8 @@
+from math import sqrt
+mx = 10
+pytha = [(a, b, sqrt(a**2 + b**2)) for a in range(1, mx) for b in range(a, mx)]
+
+# Filter out those that are not integers
+pytha = list(filter(lambda numb:numb[2].is_integer(), pytha))
+
+print(pytha)
