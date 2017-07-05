68fa73176534c81e95730eb7b02ae343c572afc1
Abou Daman <adaman2000@gmail.com>
2017-07-05 15:26:15 -0400

    Updated comments

--- comprpytha.py
+++ comprpytha.py
@@ -5,4 +5,5 @@ pytha = [(a, b, sqrt(a**2 + b**2)) for a in range(1, mx) for b in range(a, mx)]
 # Filter out those that are not integers
 pytha = list(filter(lambda numb:numb[2].is_integer(), pytha))
 
+#Outputs the result
 print(pytha)
