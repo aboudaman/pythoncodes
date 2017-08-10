d59b572ba2e26a4cb76583a2cc312c268672f05c
aboudaman <adaman2000@gmail.com>
2017-08-10 16:29:16 -0400

    updated algorithm

---
+++ p_test.py
@@ -0,0 +1,15 @@
+my_name = "Abou"
+price = 23.445533
+print(f"{my_name}, what's up")
+print(round(price, 2))
+
+joke = "this is funny {}"
+print(joke.format(my_name))
+
+print("*" * 20)
+print(2 ** 4)
+a = 'boo'
+b = "cool"
+
+print(a, end=' ')
+print(b)
